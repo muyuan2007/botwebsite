d1370afb30a7690c4d7e460d31a409d9c0f3868d
Muyuan Liu <96789081+muyuan2007@users.noreply.github.com>
2026-01-08 18:08:29 -0500

    Deleted filler file

--- pages/stupidshit.js
+++
@@ -1,3 +0,0 @@
-export default function Stupid (props) {
-    return <input id='hi' type='color' style={{position: 'absolute', top: '50%', left: '50%'}} onChange={() => {console.log(document.getElementById('hi').value)}}/>
-}
